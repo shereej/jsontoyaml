fe4f98826517aac795572e4bc3e6b0af04207955
shereej <pshereej@gmail.com>
2018-01-30 14:33:45 +0530

    Add files via upload

--- convert2yaml.js
+++ convert2yaml.js
@@ -1,24 +1,15 @@
 
 var fs = require('fs');
-//var YAML = require('json2yaml');
-//var util = require('util');
-var yaml = require('write-yaml');
-var obj, ymlText, dObj;
+const yamlconv = require('json2yaml');
+const jsonToYaml = require('json2yaml');
+var obj, ymlText;
 
 
     obj = JSON.parse(fs.readFileSync('file.json', 'utf8'));    
-    var length = JSON.stringify(obj).length;
-    obj.newsize = length;
-    //ymlText = YAML.stringify(obj);   
-    //obj = util.inspect(obj); 
-    //console.log(obj);
-    //console.log(JSON.stringify(obj,null,4));
+    obj.forEach(obj => {
+        obj["objsize"] = JSON.stringify(obj).length;
+    });
+    var ymlText = yamlconv.stringify(obj);
+    fs.writeFileSync("yaml.yaml", ymlText);
     
-
-    fs.writeFileSync('dt.json', JSON.stringify(obj,null,4) , 'utf-8');
-    //dObj = JSON.parse(fs.readFileSync('data.json', 'utf8')); 
-
-    yaml('dat.yml', obj, function(err) {  });
-
-
 
